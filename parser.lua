16be7fff51724c1edb14d40765107a3a72880108
potezy <leonloi3278@gmail.com>
2017-03-08 15:33:11 -0500

    good start kappa

--- parser.lua
+++ parser.lua
@@ -1,14 +1,26 @@
+require 'transformations'
+
+
+function string:split(sep) --credit to lua user manual
+    local sep, fields = sep or ":", {}
+    local pattern = string.format("([^%s]+)", sep)
+    self:gsub(pattern, function(c) fields[#fields+1] = c end)
+    return fields
+ end
+
+
+
 function parseFile(f)
-	 file = io.open(f , "r")
 	 local lines = {}
-	 for line in io.lines() do
+	 for line in io.lines(f) do
 	     table.insert(lines, line)
-	     print(1)
 	 end
-	 io.close(file)
-	 for i , l in ipairs(lines) do
-	     print(l , "\n")
-	 end
+	 
+	 print((lines[1]:split(" "))[1])
+	 
+	 
+	 
+
 end
 
 parseFile("commands")
